066cbd95b1e243fd7d55fce41ef347a1d9da298a
Jakub Tichý <tichyj15@fit.cvut.cz>
2022-11-26 00:53:39 +0100

    homepage - padding, unfinished list

--- lib/main.dart
+++ lib/main.dart
@@ -31,18 +31,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -112,38 +100,44 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           //mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              SizedBox(
-                height: 50,
-                width: 250,
-                child: DateTimeFormField(
-                  decoration: const InputDecoration(
-                    hintStyle: TextStyle(color: Colors.black45),
-                    errorStyle: TextStyle(color: Colors.redAccent),
-                    border: OutlineInputBorder(),
-                    suffixIcon: Icon(Icons.event_note),
-                    labelText: 'Pick a date and time',
-                  ),
-                  mode: DateTimeFieldPickerMode.dateAndTime,
-                  autovalidateMode: AutovalidateMode.always,
-                  validator: (e) =>
-                      (e?.day ?? 0) == 1 ? 'Please not the first day' : null,
-                  onDateSelected: (DateTime value) {
-                    print(value);
-                  },
-                ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 0.0),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.5,
+                  child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
+                    SizedBox(
+                      height: 50,
+                      width: 250,
+                      child: DateTimeFormField(
+                        decoration: const InputDecoration(
+                          hintStyle: TextStyle(color: Colors.black45),
+                          errorStyle: TextStyle(color: Colors.redAccent),
+                          border: OutlineInputBorder(),
+                          suffixIcon: Icon(Icons.event_note),
+                          labelText: 'Pick a date and time',
+                        ),
+                        mode: DateTimeFieldPickerMode.dateAndTime,
+                        autovalidateMode: AutovalidateMode.always,
+                        validator: (e) =>
+                            (e?.day ?? 0) == 1 ? 'Please not the first day' : null,
+                        onDateSelected: (DateTime value) {
+                          print(value);
+                        },
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                      width: 250,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Enter a search term',
+                        ),
+                      ),
+                    )
+                  ]),
               ),
-              const SizedBox(
-                height: 50,
-                width: 250,
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter a search term',
-                  ),
-                ),
-              )
-            ]),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
@@ -170,7 +164,29 @@ class _MyHomePageState extends State<MyHomePage> {
                       ],
                     ))
               ],
-            )
+            ),
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 0.0),
+            //   child:
+            //     GridView.count(
+            //       // Create a grid with 2 columns. If you change the scrollDirection to
+            //       // horizontal, this produces 2 rows.
+            //       crossAxisCount: 2,
+            //       // Generate 100 widgets that display their index in the List.
+            //       children: List.generate(100, (index) {
+            //         return SizedBox(
+            //           height: 300,
+            //           width: MediaQuery.of(context).size.width/3,
+            //           child: Center(
+            //             child: Text(
+            //               'Item $index',
+            //               style: Theme.of(context).textTheme.headline5,
+            //             ),
+            //           ),
+            //         );
+            //       }),
+            //     ),
+            // )
           ],
         ),
       ),
