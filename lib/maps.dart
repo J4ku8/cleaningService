e6d2478fb117dc03a8349854cc9f99f7c41e9c97
romaniusin <55702283+romaniusin@users.noreply.github.com>
2022-11-30 11:48:32 +0100

    Add points

--- lib/maps.dart
+++ lib/maps.dart
@@ -6,8 +6,19 @@ import 'dart:ui' as ui;
 
 class GoogleMap extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context,) {
     String htmlId = "7";
+    List<LtdLng> positions = [
+      new LtdLng(20.0, 10.0),
+      new LtdLng(20.0, 10.0),
+      new LtdLng(0001.00, 000.1111),
+      new LtdLng(20.0, 12.0),
+      new LtdLng(0021.00, 000.1111),
+      new LtdLng(0000.00, 000.1111),
+      new LtdLng(0000.00, 000.1111),
+      new LtdLng(20.0, 10.0),
+      new LtdLng(0021.00, 000.1111),
+    ]
 
     // ignore: undefined_prefixed_name
     ui.platformViewRegistry.registerViewFactory(htmlId, (int viewId) {
@@ -22,6 +33,15 @@ class GoogleMap extends StatelessWidget {
         ..style.border = 'none';
 
       final map = GMap(elem, mapOptions);
+
+      positions.forEach(() => {
+        Marker(MarkerOptions()
+          ..icon = _icon
+          ..position = gMap.LatLng(35.7560423, 139.7803552)
+          ..map = map
+          );
+      })
+
       return elem;
     });
 
