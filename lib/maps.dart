51247f92aa715c2c4b7460d7e62e367ca76fc7c3
Roman Soběslav <sobesrom@fit.cvut.cz>
2022-12-05 22:20:07 +0100

    Add map markers

--- lib/maps.dart
+++ lib/maps.dart
@@ -5,15 +5,26 @@ import 'package:google_maps/google_maps.dart';
 import 'dart:ui' as ui;
 
 class GoogleMap extends StatelessWidget {
+  const GoogleMap({super.key});
+
   @override
   Widget build(BuildContext context) {
     String htmlId = "7";
+    LatLng center = LatLng(50.105092, 14.389680);
+
+    List<LatLng> positions = [
+      LatLng(50.105092, 14.389680),
+      LatLng(50.106092, 14.389680),
+      LatLng(50.104092, 14.389680),
+      LatLng(50.105092, 14.390680),
+      LatLng(50.105092, 14.388680),
+    ];
 
     // ignore: undefined_prefixed_name
     ui.platformViewRegistry.registerViewFactory(htmlId, (int viewId) {
       final mapOptions = MapOptions()
         ..zoom = 15
-        ..center = LatLng(50.105092, 14.389680);
+        ..center = center;
 
       final elem = DivElement()
         ..id = htmlId
@@ -22,6 +33,17 @@ class GoogleMap extends StatelessWidget {
         ..style.border = 'none';
 
       final map = GMap(elem, mapOptions);
+
+      for (var element in positions) {
+        var marker = Marker(MarkerOptions()
+          ..position = element
+          ..map = map
+          ..title = 'test'
+          ..clickable = true);
+        final infoWindow = InfoWindow(InfoWindowOptions()..content = 'test');
+        marker.onClick.listen((event) => infoWindow.open(map, marker));
+      }
+
       return elem;
     });
 
